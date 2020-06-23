3453b05229eac99622ab4ecafcb438302af11eec
Kehan Guo <kehanguo@55-78.resnet-wifi.uni.edu>
2020-06-23 11:50:03 -0500

    completed first part of lab1

---
+++ lab1/HelloNumber.java
@@ -0,0 +1,14 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0;
+        int a =0;
+        while (x < 10) {
+            
+            a=a+x;
+            System.out.print(a 
+                             + " ");
+            
+            x = x + 1;
+        }
+    }
+}
